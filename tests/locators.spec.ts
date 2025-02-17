ed88a45268b7ecde2259b47b248c8a50477731d0
amit <mr.ahr.es6@gmail.com>
2025-02-18 02:12:03 +0600

    locators

--- tests/locators.spec.ts
+++ tests/locators.spec.ts
@@ -47,3 +47,22 @@ await page.getByPlaceholder("Password").fill('1234')
 
 
 });
+
+
+test("Built-in Locators", async ({page}) => {
+    
+  await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+
+  const logo =  await page.getByAltText("company-branding")
+  await expect(logo).toBeVisible()
+
+  await page.getByPlaceholder('Username').fill("Admin")
+  await page.getByPlaceholder('Password').fill("admin123")
+
+  await page.getByRole('button', { name: 'Login' }).click()
+
+  await expect( page.getByText('First Name TEST Last Name TEST')).toBeVisible();
+
+
+
+})
